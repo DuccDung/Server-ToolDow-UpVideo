3d4aee2e47298db886efef593c60c8e2e4a94f3c
DuccDung <duccdung999@gmail.com>
2025-06-08 01:28:23 +0700

    fix date: 8/6

--- Server-ToolDow&UpVideo/obj/Debug/net8.0/Server-ToolDow&UpVideo.AssemblyInfo.cs
+++ Server-ToolDow&UpVideo/obj/Debug/net8.0/Server-ToolDow&UpVideo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server-ToolDow&UpVideo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40488d129510681b1fce2cdf3ef036c24b916982")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7581860385ddf4bb62a7a05534eb6955dbe107a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server-ToolDow&UpVideo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server-ToolDow&UpVideo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
